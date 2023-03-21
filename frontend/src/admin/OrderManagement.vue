8967e38255d5d6e359db68ce3daf3fd2f3dda449
Biffin Varghese <biffinvarghese@mca.ajce.in>
2023-03-21 09:46:21 +0530

    update ordermanagement

--- frontend/src/admin/OrderManagement.vue
+++ frontend/src/admin/OrderManagement.vue
@@ -5,51 +5,33 @@
         <tr>
           <th>Table Type</th>
           <th>Table Name</th>
-          <th>Seats</th>
-          <th>Description</th>
-          <th>Image</th>
-          <th>Action</th>
-          <th>Action</th>
+          <th>Booking Time From</th>
+          <th>User Phone Number</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(tableInfo, index) in tablesData" :key="index">
-          <td>{{ tableInfo.table_type }}</td>
-          <td>{{ tableInfo.table_name }}</td>
-          <td>{{ tableInfo.count }}</td>
-          <td>{{ tableInfo.table_description }}</td>
+        <tr
+          v-for="(tableInfo, index) in tablesData"
+          :key="index"
+          @click="
+            $router.push({
+              path: '/admin/dashboard/order-details',
+              query: { id: tableInfo.id },
+            })
+          "
+        >
           <td>
-            <img
-              :src="'http://localhost:8081/' + tableInfo.table_image"
-              alt=""
-              style="width: 10vw"
-            />
+            {{ tableInfo?.table_details?.table_type || `Parcel ${index + 1}` }}
           </td>
+          <td>{{ tableInfo?.table_details?.table_name || "" }}</td>
           <td>
-            <button
-              class="btn"
-              @click.prevent="
-                $router.push({
-                  path: '/admin/dashboard/table-add-or-edit',
-                  query: {
-                    table_id: tableInfo.table_id,
-                  },
-                })
-              "
-            >
-              Edit
-            </button>
+            {{
+              new Date(
+                tableInfo?.table_details?.date || new Date()
+              )?.toLocaleString() || ""
+            }}
           </td>
-          <td>
-            <button
-              class="btn"
-              @click.prevent="
-                deleteTableConfig(tableInfo.table_id)
-              "
-            >
-              Delete
-            </button>
-          </td>
+          <td>{{ tableInfo.user.user_phone }}</td>
         </tr>
       </tbody>
     </table>
@@ -70,17 +52,38 @@ export default {
   methods: {
     async fetchBookedTables() {
       try {
-        const response = await axios.get("book-tables");
-        this.tablesData = response.data;
-        console.log("error", response);
+        const tablesData = [];
+        const response = await axios.get("orders");
+        const tables = response?.data || [];
+        if (tables && tables.length > 0) {
+          for (let index = 0; index < tables.length; index++) {
+            console.log("log");
+            const table = tables[index];
+            table.table_details = JSON.parse(table?.table_details || "{}");
+            const res = await axios.get("users/id/" + table.user_id);
+            table.user = res.data;
+            tablesData.push(table);
+          }
+        }
+        this.tablesData = tablesData;
+        // this.tablesData =
+        //   this.tablesData && this.tablesData.length > 0
+        //     ? this.tablesData.map((table) => {
+        //         table.table_details = JSON.parse(table?.table_details || "{}");
+        //         const res = axios.get("users/id/" + table.user_id);
+        //         table.user = res.data;
+        //         return table;
+        //       })
+        //     : [];
+        console.log("error", this.tablesData);
       } catch (error) {
         console.log("error", error.data);
       }
     },
     async deleteTableConfig(id) {
       try {
-        const response = await axios.delete("book-tables/"+id);
-        await this.fetchBookedTables()
+        const response = await axios.delete("book-tables/" + id);
+        await this.fetchBookedTables();
         console.log("error", response);
       } catch (error) {
         console.log("error", error.data);
