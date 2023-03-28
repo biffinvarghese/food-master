d8232ee0ad93f6944eccae72a4cec1b0743f2f3a
Biffin Varghese <biffinvarghese@mca.ajce.in>
2023-03-28 11:19:41 +0530

    update navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -7,6 +7,8 @@
       <nav class="navbar" v-if="!isAdmin">
         <router-link @click="scrollToTop()" to="/">home</router-link>
         <router-link @click="scrollToTop()" to="/about">about</router-link>
+        <router-link @click="review()" to="" target="_blank" v-if="user">review site</router-link>
+        <router-link @click="nutrient()" to="" target="_blank" v-if="user">nutrient check</router-link>
         <router-link @click="scrollToTop()" to="/menu" v-if="user"
           >menu</router-link
         >
@@ -87,6 +89,12 @@
       scrollToTop() {
         window.scrollTo(0, 0);
       },
+      review(){
+            window.location.replace("http://192.168.54.84:3000/");
+        },
+        nutrient(){
+            window.location.replace("http://192.168.54.84:3001/");
+        },
       showNav: function () {
         let navbar = document.querySelector(".header .navbar");
         navbar.classList.toggle("active");
